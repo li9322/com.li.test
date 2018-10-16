793c0281fd8db25a5aeb880193ba88401e6c08a2
li <1172892394@qq.com>
2018-10-16 14:43:02 +0800

    this is to test

--- src/main/scala/com/li/spark/scala/wordcount/Testclass.scala
+++ src/main/scala/com/li/spark/scala/wordcount/Testclass.scala
@@ -1,5 +1,12 @@
 package com.li.spark.scala.wordcount
 
-object Testclass {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object Testclass {
+  def main(args: Array[String]): Unit = {
+    val config=new SparkConf()
+    val sc=new SparkContext(config)
+    sc.textFile(args(0)).map(x=>{(x.split(" ")(0),1)}).reduceByKey(_+_).collect()
+    sc.stop()
+  }
 }
